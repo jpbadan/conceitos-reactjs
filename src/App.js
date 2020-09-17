982b7f61223b34f01e16c4518262196fc7eda6f4
jpbadan <jpbadan@gmail.com>
2020-09-17 18:39:28 +0200

    Added funcionality to add and delete projects

--- src/App.js
+++ src/App.js
@@ -4,32 +4,56 @@ import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    // setRepositories([... repositories, ])
+    const newRepository = {
+      "title": "Projeto sem titulo",
+      "url": "",
+      "techs": "[]"
+    };
+
+    const response = await api.post('repositories', newRepository);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // console.log(id);
+    await api.delete(`repositories/${id}`);
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    
+    repositories.splice(repositoryIndex, 1);
+
+    setRepositories([...repositories]);
   }
 
-  const [repositories, setRepositories] = useState([]);
-
-  // setRepositories([[],repositories]);
 
   return (
     <div>
       <ul data-testid="repository-list">
         {repositories.map(repository => 
           <li key = {repository.id}>
+
               {repository.title}
-              <button onClick={() => handleRemoveRepository(1)}>
+
+              <button onClick={() => handleRemoveRepository(repository.id)}>
                 Remover
               </button>
           </li>
           )}
         
       </ul>
-
+      {/* <input type="text" id="repositoryTitle" size="25"/> */}
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
